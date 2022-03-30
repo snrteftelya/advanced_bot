c720a049818ecc8c027f7cadb9b2fba46c3cdc67
snrteftelya <102659963+snrteftelya@users.noreply.github.com>
2022-03-30 14:31:15 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-token = "5087965750:AAEWP9ydFBItfsLOd0I5HlRsRXq5SBF8K-U"
-open_weather_token = "2da4fc6a164a9350dcdfdc45a8991433"
-kolya = 644784412
-#mum = 1444393975
-olya = 970854545
-#dasha = 905860668
-#nikita = 993748229
